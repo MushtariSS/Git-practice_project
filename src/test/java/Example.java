c0fc4ad8c5c028f75048afe518c5d686403d9df5
XPL <mushti.romin@gmail.com>
2024-09-24 08:08:03 -0400

    first github project

---
+++ src/test/java/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    public static void main(String [] args){
+        int age = 20;
+        String name = "John";
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
